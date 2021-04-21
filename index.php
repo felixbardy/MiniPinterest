0c993d0dbfe7e72f0168cbe6017cc7a9df32c16f
BARDY FELIX p1925107 <felix.bardy@etu.univ-lyon1.fr>
2021-04-21 04:21:50 +0200

    Projet de base

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Projet BDW1 2021</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        Hello, world!
+    </body>
+</html>
