8cc1b3701660f0bb538eb8a9f4b2a90fe6bdbda2
BARDY FELIX p1925107 <felix.bardy@etu.univ-lyon1.fr>
2021-04-22 01:40:32 +0200

    Séléction de la catégorie à afficher

--- index.php
+++ index.php
@@ -13,6 +13,22 @@
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        Hello, world!
+    <form name="select_category" action="" method="GET">
+      <select name="category" id="category">
+        <option value="all">Toutes les photos</option>
+        <?php
+            $categories = getAllCategories($_SESSION["connection"]);
+            foreach($categories as $category)
+            {
+              // Si la catégorie a été séléctionée, l'afficher comme telle
+              if (!is_null($_GET["category"]) && $_GET["category"] == $category["catId"])
+                echo "<option value=" . strval($category["catId"]) . " selected>" . $category["nomCat"] . "</option>\n";
+              else
+                echo "<option value=" . strval($category["catId"]) . ">" . $category["nomCat"] . "</option>\n";
+            }
+        ?>
+      </select>
+      <input type="submit" value="Filtrer">
+    </form>
     </body>
 </html>
