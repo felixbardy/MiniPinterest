147415c0104175d4c19279ebd095a2abe99c3483
BARDY FELIX p1925107 <felix.bardy@etu.univ-lyon1.fr>
2021-04-30 18:40:23 +0200

    Commenntaire

--- func/bd.php
+++ func/bd.php
@@ -38,7 +38,7 @@ message d'erreur est affiché.*/
 function executeUpdate($link, $query)
 {
 	if 
-	( 	// Si il n'y a aucun de ces mots clé
+	( 	// Si il n'y a aucun de ces mots clé en début de requête
 		   strpos($query, 'INSERT') != 0 
 		&& strpos($query, 'UPDATE') != 0 
 	  	&& strpos($query, 'DELETE') != 0
