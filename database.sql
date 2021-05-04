2fe1cb39967f2f914c84b82bf1f4f8a8a417dc05
BARDY FELIX p1925107 <felix.bardy@etu.univ-lyon1.fr>
2021-05-04 16:51:45 +0200

    Version finale de la base de données

--- database.sql
+++ database.sql
@@ -2,10 +2,10 @@
 -- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: May 01, 2021 at 12:05 PM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 7.3.27
+-- Hôte : localhost
+-- Généré le : mar. 04 mai 2021 à 16:51
+-- Version du serveur :  10.4.18-MariaDB
+-- Version de PHP : 8.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `images`
+-- Base de données : `images`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Categorie`
+-- Structure de la table `Categorie`
 --
 
 CREATE TABLE `Categorie` (
@@ -33,133 +33,143 @@ CREATE TABLE `Categorie` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Cette table contient les catégories des images';
 
 --
--- Dumping data for table `Categorie`
+-- Déchargement des données de la table `Categorie`
 --
 
 INSERT INTO `Categorie` (`catId`, `nomCat`) VALUES
-(1, 'banc'),
-(2, 'bibliothèque'),
-(3, 'buffet'),
-(4, 'caisses'),
-(5, 'chaise'),
-(6, 'cheminée'),
-(7, 'escalier'),
-(8, 'lit'),
-(9, 'râtelier'),
-(10, 'table'),
-(11, 'trappe');
+(1, 'paysage'),
+(2, 'bateau'),
+(3, 'squelette'),
+(4, 'jeux vidéos'),
+(6, 'animal'),
+(12, 'espace'),
+(13, 'la catégorie vide');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Photo`
+-- Structure de la table `Photo`
 --
 
 CREATE TABLE `Photo` (
   `photoId` int(11) NOT NULL COMMENT 'L''identifiant de la photo',
   `nomFich` varchar(250) CHARACTER SET utf8 NOT NULL COMMENT 'Le nom du fichier correspondant',
   `description` varchar(250) CHARACTER SET utf8 NOT NULL COMMENT 'La description de la photo',
-  `catId` int(11) NOT NULL COMMENT 'L''Id de la catégorie de la photo'
+  `catId` int(11) NOT NULL COMMENT 'L''Id de la catégorie de la photo',
+  `auteur` varchar(100) CHARACTER SET utf8 DEFAULT NULL COMMENT 'L''utilisateur ayant ajouté la photo',
+  `hidden` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Indique si la photo est cachée'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Une photo';
 
 --
--- Dumping data for table `Photo`
+-- Déchargement des données de la table `Photo`
 --
 
-INSERT INTO `Photo` (`photoId`, `nomFich`, `description`, `catId`) VALUES
-(2, 'banc1.png', 'un banc en bois horizontal de 3 cases de long', 1),
-(3, 'biblio1.png', 'Une bibliothèque avec un livre ouvert (3 cases)', 2),
-(4, 'biblio2.png', 'Une bibliothèque avec livres et parchemins (3 cases)', 2),
-(5, 'buffet1.png', 'Un buffet poussiéreux rempli de matériaux ésotériques (3 cases)', 3),
-(6, 'buffet2.png', 'Un buffet rouge fermé (3 cases)', 3),
-(7, 'caisses1.png', 'Un tas de 4 caisses (1 case)', 4),
-(8, 'chaise1.png', 'Une chaise parée de dorures avec un coussin rouge (1 case)', 5),
-(9, 'chaise2.png', 'Une modeste chaise en bois (1 case)', 5),
-(10, 'chaise3.png', 'Une chaise en métal très metal (1 case)', 5),
-(11, 'chaise4.png', 'Une chaise parée de dorures avec un coussin rouge au centre d\'un tapis décomposé (2x2 cases)', 5),
-(12, 'cheminee1.png', 'Une cheminée protégée en longueur (4 cases)', 6),
-(13, 'cheminee2.png', 'Une cheminée élaborée (2x4 cases)', 6),
-(14, 'escalier1.png', 'Un escalier en colimaçon en pierre allant vers le bas (2x2 cases)', 7),
-(15, 'escalier2.png', 'Un escalier classique descendant vers une grande porte (2x2 cases)', 7),
-(16, 'lit1.png', 'Un lit en bois avec matelas, oreiller et couverture grise (2 cases)', 8),
-(17, 'lit2.png', 'Un lit en bois avec matelat, oreiller gris et couverture à carreaux (2 cases)', 8),
-(18, 'lit3.png', 'Un lit double en bois sur paille avec 2 oreillers marrons (2x2 cases)', 8),
-(19, 'lit4.png', 'Un sac de couchage en cuir (2 cases)', 8),
-(20, 'ratelier1.png', 'Un râtelier à armes contenant 2 glaives, 1 hache et 1 bouclier (3 cases)', 9),
-(21, 'ratelier2.png', 'Un râtelier à armes contenant 1 masse à 1 main, 2 haches, 1 marteau, 1 masse à 2 mains et une masse à pics (3 cases)', 9),
-(22, 'table1.png', 'Une table couverte d\'objets évoquant la magie noire (2x3 cases)', 10),
-(23, 'table2.png', 'Une table en bon état avec du matériel d\'alchimie (2x3 cases)', 10),
-(24, 'table3.png', 'Une table avec un équipement classique d\'aventurier: corde, protections, épée, couteau, arbalète, carquois, carte et sac à dos (2x3 cases)', 10),
-(25, 'table4.png', 'Une table de taverne peu recommendable: couteau, 2 choppes renversées, 1 os et 1 bourse (2x3 cases)', 10),
-(26, 'table5.png', 'Table de taverne: 1 pichet, 2 choppes, 1 bougie, 1 bourse et 1 couteau (2x3 cases)', 10),
-(27, 'table6.png', 'Table carée d\'herbologie (2x2 cases)', 10),
-(28, 'tonneaux1.png', '3 tonneaux (1 case)', 4),
-(29, 'trappe1.jpg', 'Une trappe vers une fosse à pieux (nxn cases)', 11),
-(30, 'trappe2.jpg', 'Une trappe vers un trou sans fond (4x4 cases)', 11),
-(31, 'trappe3.png', 'Un piège à rondin à pics (2x2 cases)', 11),
-(32, 'trappe4.png', 'Un portail noir vers une créature tentaculaire (2x3 cases)', 11),
-(33, 'trappe5.png', 'Un puit de lave (2x2 cases)', 11),
-(34, 'trappe6.png', 'Une flaque d\'acide (2x2 cases)', 11),
-(35, 'trappe7.png', 'Une fosse dont on aperçoit le fond (nxn cases)', 11);
+INSERT INTO `Photo` (`photoId`, `nomFich`, `description`, `catId`, `auteur`, `hidden`) VALUES
+(38, 'DSC37.png', 'Une planète rouge entourée de galaxies', 12, 'Eggman', 0),
+(39, 'DSC39.png', 'La Terre dans une jarre', 12, 'Eggman', 0),
+(40, 'DSC40.png', 'Un pont entre 2 étoiles', 12, 'Eggman', 1),
+(41, 'DSC41.png', 'La Terre plate', 12, 'Eggman', 0),
+(42, 'DSC42.png', 'Chevalier chevauchant une vache dans l\'espace', 12, 'Eggman', 0),
+(43, 'DSC43.png', 'Un Kraken détruit la Terre', 12, 'Eggman', 0),
+(44, 'DSC44.png', 'La Terre en forme de diplodocus', 12, 'Eggman', 0),
+(45, 'DSC45.png', 'Un coucher de soleil avec un phare', 1, 'admin', 0),
+(46, 'DSC46.png', 'Forêt de sapins en couleurs sépia', 1, 'admin', 0),
+(47, 'DSC47.png', 'Le Mordor, avec l\'œuil de sauron au milieu', 1, 'admin', 0),
+(48, 'DSC48.png', 'Phare dans la soirée', 1, 'admin', 0),
+(49, 'DSC49.png', 'Soleil dans les nuages', 1, 'admin', 0),
+(50, 'DSC50.png', 'Un cheval avec quelques jambes de trop', 6, 'admin', 0),
+(51, 'DSC51.png', 'Un bébé dinosaure', 6, 'admin', 0),
+(52, 'DSC52.png', 'Un canard cyborg', 6, 'admin', 0),
+(53, 'DSC53.png', 'Une girafe clairement sur le point de tomber', 6, 'admin', 0),
+(54, 'DSC54.png', 'Une chouette-terminator (terminatowl)', 6, 'admin', 0),
+(55, 'DSC55.png', 'Un oiseau qui aime l\'hiver', 6, 'admin', 0),
+(56, 'DSC56.png', 'Billy, un personnage du jeu Karlson', 4, 'admin', 0),
+(57, 'DSC57.png', 'Des personnage du jeu Amon Us en train de dessiner', 4, 'Eggman', 1),
+(58, 'DSC58.png', 'Un ours dans un carton', 6, 'Eggman', 0),
+(59, 'DSC59.png', 'Hornet, un personnage du jeu Hollow Knight', 4, 'Eggman', 0),
+(60, 'DSC60.png', 'Un pacman en 3D coincé par un fantôme rouge', 4, 'Eggman', 0),
+(61, 'DSC61.png', 'Deux étoiles avec des visages sourient', 12, 'Eggman', 0),
+(62, 'DSC62.png', 'Dromadaire habillé en Rick Astley', 6, 'admin', 0),
+(63, 'DSC63.png', 'Un squelette essayant de boire', 3, 'admin', 0),
+(64, 'DSC64.png', 'Un squelette travaillant en tant que serveur', 3, 'admin', 1),
+(65, 'DSC65.png', 'Un squelette agacé de recevoir des os sur le crâne', 3, 'admin', 0),
+(66, 'DSC66.png', 'Un poisson TIRE SON LASEEEEEEEEEEEER', 6, 'admin', 1),
+(67, 'DSC67.png', 'Un bateau-taco', 2, 'Eggman', 0),
+(68, 'DSC68.png', 'Un bateau volant, traversant les nuages', 2, 'Eggman', 0),
+(69, 'DSC69.gif', 'Kirbo, un personnage de TerminalMontage, triste', 4, 'admin', 0),
+(70, 'DSC70.png', 'Beaucoup (trop) de souris', 6, 'Eggman', 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `User`
+-- Structure de la table `User`
 --
 
 CREATE TABLE `User` (
   `pseudo` varchar(100) NOT NULL COMMENT 'Le pseudonyme de l''utilisateur',
-  `passwordHash` varchar(60) NOT NULL COMMENT 'Le Hash du mot de passe de l''utilisateur'
+  `passwordHash` varchar(60) NOT NULL COMMENT 'Le Hash du mot de passe de l''utilisateur',
+  `admin` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Indique si l''utilisateur est administrateur'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Les utilisateurs du site';
 
 --
--- Indexes for dumped tables
+-- Déchargement des données de la table `User`
+--
+
+INSERT INTO `User` (`pseudo`, `passwordHash`, `admin`) VALUES
+('admin', '$2y$10$dCig0vdT1jmhKC8V95i6w.UvfZuXAxH0zonPdSoF3vzJN5ZUSBXF2', 1),
+('Eggman', '$2y$10$gM/hmHBQB.90sPYozzTLv.LTsWCMfC7Gpc.VCSHG2X64yreEyJtv6', 0),
+('Félix', '$2y$10$JPbGlh1uAjte1oGlEF5hheZFnGUVgnRPB9dhavikLjDrFTvd0pFda', 0);
+
+--
+-- Index pour les tables déchargées
 --
 
 --
--- Indexes for table `Categorie`
+-- Index pour la table `Categorie`
 --
 ALTER TABLE `Categorie`
   ADD PRIMARY KEY (`catId`);
 
 --
--- Indexes for table `Photo`
+-- Index pour la table `Photo`
 --
 ALTER TABLE `Photo`
   ADD PRIMARY KEY (`photoId`),
-  ADD UNIQUE KEY `photoId` (`photoId`);
+  ADD UNIQUE KEY `photoId` (`photoId`),
+  ADD KEY `Category must exist` (`catId`),
+  ADD KEY `Author must exist` (`auteur`);
 
 --
--- Indexes for table `User`
+-- Index pour la table `User`
 --
 ALTER TABLE `User`
   ADD UNIQUE KEY `Pseudo` (`pseudo`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT for table `Categorie`
+-- AUTO_INCREMENT pour la table `Categorie`
 --
 ALTER TABLE `Categorie`
-  MODIFY `catId` int(11) NOT NULL AUTO_INCREMENT COMMENT 'L''identifiant de la catégorie', AUTO_INCREMENT=12;
+  MODIFY `catId` int(11) NOT NULL AUTO_INCREMENT COMMENT 'L''identifiant de la catégorie', AUTO_INCREMENT=14;
 
 --
--- AUTO_INCREMENT for table `Photo`
+-- AUTO_INCREMENT pour la table `Photo`
 --
 ALTER TABLE `Photo`
-  MODIFY `photoId` int(11) NOT NULL AUTO_INCREMENT COMMENT 'L''identifiant de la photo', AUTO_INCREMENT=36;
+  MODIFY `photoId` int(11) NOT NULL AUTO_INCREMENT COMMENT 'L''identifiant de la photo', AUTO_INCREMENT=73;
 
 --
--- Constraints for dumped tables
+-- Contraintes pour les tables déchargées
 --
 
 --
--- Constraints for table `Photo`
+-- Contraintes pour la table `Photo`
 --
 ALTER TABLE `Photo`
+  ADD CONSTRAINT `Author must exist` FOREIGN KEY (`auteur`) REFERENCES `User` (`pseudo`) ON DELETE SET NULL,
   ADD CONSTRAINT `Category must exist` FOREIGN KEY (`catId`) REFERENCES `Categorie` (`catId`) ON DELETE NO ACTION;
 COMMIT;
 
